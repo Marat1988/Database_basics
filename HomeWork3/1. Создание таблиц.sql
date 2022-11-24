5fae7e91bd372eeb42b4d438307e1f797d3fb8af
Marat <108996479+Marat1988@users.noreply.github.com>
2022-11-24 12:29:22 +0200

    HomeWork3

---
+++ HomeWork3/1. Создание таблиц.sql
@@ -0,0 +1,91 @@
+﻿/*Создание БД*/
+CREATE DATABASE Academy;
+GO
+
+USE Academy;
+GO
+
+/*Создание таблиц*/
+CREATE TABLE Departments
+(
+	Id INT IDENTITY(1,1),
+	Financing MONEY NOT NULL CONSTRAINT DF_Financing DEFAULT(0),
+	Name NVARCHAR(100) NOT NULL
+	CONSTRAINT PK_Departments_Id PRIMARY KEY (Id),
+	CONSTRAINT UQ_Departments_Name UNIQUE (Name),
+	CONSTRAINT CK_Departments_Financing CHECK (Financing >= 0),
+);
+GO
+
+CREATE TABLE Faculties
+(
+	Id INT IDENTITY(1,1),
+	Dean NVARCHAR(MAX) NOT NULL,
+	Name NVARCHAR(100) NOT NULL
+	CONSTRAINT PK_Faculties_Id PRIMARY KEY (Id),
+	CONSTRAINT CK_Faculties_Dean CHECK (Dean<>''),
+	CONSTRAINT UQ_Faculties_Name UNIQUE (Name),
+	CONSTRAINT CK_Faculties_Name CHECK (Name<>'')
+);
+GO
+
+CREATE TABLE Groups
+(
+	Id INT IDENTITY(1,1),
+	Name NVARCHAR(10) NOT NULL,
+	Rating INT NOT NULL,
+	Year INT NOT NULL
+	CONSTRAINT PK_Groups_Id PRIMARY KEY (Id),
+	CONSTRAINT UQ_Groups_Name UNIQUE (Name),
+	CONSTRAINT CK_Groups_Name CHECK (Name<>''),
+	CONSTRAINT CK_Groups_Rating CHECK (Rating BETWEEN 0 AND 5),
+	CONSTRAINT CK_Groups_Year CHECK (Year BETWEEN 1 AND 5),
+);
+GO
+
+CREATE TABLE Teachers
+(
+	Id INT IDENTITY(1,1),
+	EmploymentDate DATE NOT NULL,
+	IsAssistant BIT NOT NULL CONSTRAINT DF_Teachers_IsAssistant DEFAULT(0),
+	IsProfessor BIT NOT NULL CONSTRAINT DF_Teachers_IsProfessor DEFAULT(0),
+	Name NVARCHAR(MAX) NOT NULL,
+	Position NVARCHAR(MAX) NOT NULL,
+	Premium MONEY NOT NULL CONSTRAINT DF_Teachers_Premium DEFAULT(0),
+	Salary MONEY NOT NULL,
+	Surname NVARCHAR(MAX) NOT NULL
+	CONSTRAINT PK_Teachers_Id PRIMARY KEY (Id),
+	CONSTRAINT CK_Teachers_EmploymentDate CHECK (EmploymentDate>='19900101'),
+	CONSTRAINT CK_Teachers_Name CHECK (Name<>''),
+	CONSTRAINT CK_Teachers_Position CHECK (Position<>''),
+	CONSTRAINT CK_Teachers_Premium CHECK (Premium>=0),
+	CONSTRAINT CK_Teachers_Salary CHECK (Salary>0),
+	CONSTRAINT CK_Teachers_Surname CHECK (Surname<>'')
+);
+GO
+/*Заполнение заблиц тестовыми данными*/
+INSERT Departments (Financing, Name)
+VALUES (10500,'Software development'),
+	   (15000, 'Mathematics'),
+	   (30000, 'Cybersecurity');
+GO
+
+INSERT Faculties (Dean, Name)
+VALUES ('Иванов И.И.', 'Разработка программного обеспечения'),
+	   ('Петров П.П.', 'Сети и Кибербезопасность');
+GO
+
+INSERT Groups (Name, Rating, Year)
+VALUES ('БВ111', 4, 5),
+	   ('БВ112', 5, 2),
+	   ('БВ113', 2, 1);
+GO
+
+INSERT Teachers (EmploymentDate, IsAssistant, IsProfessor, Name, Position, Premium, Salary, Surname)
+VALUES ('19980101', 1, 0, 'Михаил', 'Доцент', 160, 500, 'Сидоров'),
+	   ('20060110', 0, 1, 'Петр', 'Ведущий научный сотрудник', 500, 2000, 'Соловьев'),
+	   ('20080130', 1, 0, 'Александр', 'Докторант', 700, 1000, 'Орлов'),
+	   ('20090130', 1, 0, 'Кирилл', 'Младший научный сотрудник', 200, 400, 'Синицин'),
+	   ('20090130', 1, 0, 'Максим', 'Ассистент', 500, 900, 'Максименко')
+GO
+	
---
+++ HomeWork3/2. Запросы.sql
@@ -0,0 +1,91 @@
+﻿/*1) Вывести таблицу кафедр, но расположить ее поля в обратном порядке.*/
+SELECT Id, Financing, Name
+FROM Departments
+ORDER BY Id DESC;
+GO
+/*2) Вывести названия групп и их рейтинги, используя в качестве названий выводимых полей “Group Name” и “Group Rating” соответственно.*/
+SELECT Name AS [Group Name], Rating AS [Group Rating]
+FROM Groups
+ORDER BY [Name];
+GO
+/*3) Вывести для преподавателей их фамилию, процент ставки по отношению к надбавке и процент ставки по отношению к зарплате (сумма ставки и надбавки).*/
+SELECT Surname, ROUND(Salary*100/Premium,2) AS [Процент ставки по отношению к надбавке], ROUND(Salary*100/(Premium+Salary),2) AS [Процент ставки по отношению к зарплате]
+FROM Teachers
+ORDER BY Surname;
+GO
+/*4) Вывести таблицу факультетов в виде одного поля в следующем формате: “The dean of faculty [faculty] is [dean].”*/
+SELECT 'The dean of faculty ' + [Name] + ' is ' + Dean
+FROM Faculties
+ORDER BY [Name];
+GO
+/*5) Вывести фамилии преподавателей, которые являются профессорами и ставка которых превышает 1050.*/
+SELECT SurName
+FROM Teachers
+WHERE IsProfessor=1 AND Salary>1050
+ORDER BY SurName;
+GO
+/*6) Вывести названия кафедр, фонд финансирования которых меньше 11000 или больше 25000.*/
+SELECT [Name]
+FROM Departments
+WHERE Financing<11000 OR Financing>25000
+ORDER BY [Name];
+GO
+/*7) Вывести названия факультетов кроме факультета “Computer Science”.*/
+SELECT [Name]
+FROM Faculties
+WHERE [Name]<>'Computer Science'
+ORDER BY [Name];
+GO
+/*8) Вывести фамилии и должности преподавателей, которые не являются профессорами.*/
+SELECT Surname, Position
+FROM Teachers
+WHERE IsProfessor=0
+ORDER BY Surname;
+GO
+/*9) Вывести фамилии, должности, ставки и надбавки ассистентов, у которых надбавка в диапазоне от 160 до 550.*/
+SELECT Surname, Position, Salary, Premium
+FROM Teachers
+WHERE IsAssistant=1 AND Premium BETWEEN 160 AND 550
+ORDER BY Surname;
+GO
+/*10)  Вывести фамилии и ставки ассистентов.*/
+SELECT Surname, Salary
+FROM Teachers
+WHERE IsAssistant=1
+ORDER BY Surname;
+GO
+/*11) Вывести фамилии и должности преподавателей, которые были приняты на работу до 01.01.2000.*/
+SELECT Surname, Position
+FROM Teachers
+WHERE EmploymentDate<'20000101'
+ORDER BY Surname;
+GO
+/*12)  Вывести названия кафедр, которые в алфавитном порядке располагаются до кафедры “Software Development”. 
+Выводимое поле должно иметь название “Name of Department”.*/
+SELECT [Name] AS 'Name of Department'
+FROM Departments
+WHERE ASCII(SUBSTRING('Software Development', 1, 1))>ASCII(SUBSTRING(Name, 1, 1))
+ORDER BY [Name];
+GO
+/*13) Вывести фамилии ассистентов, имеющих зарплату (сумма ставки и надбавки) не более 1200.*/
+SELECT Surname
+FROM Teachers
+WHERE IsAssistant=1 AND ((Premium+Salary)<1200)
+ORDER BY Surname;
+GO
+/*14)  Вывести названия групп 5-го курса, имеющих рейтинг  в диапазоне от 2 до 4.*/
+SELECT [Name]
+FROM Groups
+WHERE [Year]=5 AND Rating BETWEEN 2 AND 4
+ORDER BY [Name];
+GO
+/*15) Вывести фамилии ассистентов со ставкой меньше 550 или надбавкой меньше 200.*/
+SELECT Surname
+FROM Teachers
+WHERE IsAssistant=1 AND (Salary<500 OR Premium<200)
+ORDER BY Surname;
+GO
+
+
+
+
